7da6910b751ceacc1bc2211f8ea02ed578f7111c
moh2010 <mohamed.hosam@gmail.com>
2019-12-12 15:34:48 +0200

    html done

--- tours/urls.py
+++ tours/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('destination', views.destination, name='destination'),
+    path('packages', views.package, name='package'),
+    path('<int:program_id>', views.tour_detail, name='tour_detail'),
+    path('search', views.search, name='search'),
+    path('contact', views.contact, name='contact'),
+
 ]
--- tours/views.py
+++ tours/views.py
@@ -6,3 +6,19 @@ def index(request):
 
 def about(request):
     return render(request, 'tours/about.html')
+
+def destination(request):
+    return render (request, 'tours/destination.html')
+
+def package(request):
+    return render (request, 'tours/packages.html')
+
+def tour_detail(request):
+    return render (request, 'tours/tour_details.html')    
+
+def search(request):
+    return render (request, 'tours/search.html')
+
+
+def contact(request):
+    return render (request, 'tours/contact.html')
