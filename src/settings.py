e0d109c14a6eb1b200dc99c619d6ab62ae66b486
moh2010 <mohamed.hosam@gmail.com>
2019-12-15 16:48:52 +0200

    change admin html & CSS

--- src/settings.py
+++ src/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'src/static')
 ]
 # Media Files
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 try:
