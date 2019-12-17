fb7589b191d7bb35e09ff042664fbee5779d6995
moh2010 <mohamed.hosam@gmail.com>
2019-12-17 14:49:16 +0200

    Upload media

--- tours/admin.py
+++ tours/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Program, Category, Destination, City
 
-#class ProgramAdmin(admin.ModelAdmin):
- #   list_display = ('',)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('id','title','arrival_date','departure_date','price_adt','flight','category','available_front_page','available_programs_page','available_lastminut_page','available_tours_page')
+    list_display_links = ('id','title','arrival_date','price_adt')
+    list_filter = ('arrival_date',)
+    list_editable = ('available_front_page','available_programs_page','available_lastminut_page','available_tours_page')
+    search_fields = ('title','description','destination','city','district','flight','hotel_name')
 
 admin.site.register(Category)
 admin.site.register(Destination)
-admin.site.register(Program)
+admin.site.register(Program, ProgramAdmin)
 admin.site.register(City)
