44b1256f63401e70c70acf79ddf67a72bde14995
[moh2010] <[mohamed.hosam@gmail.com]>
2020-02-01 16:56:20 +0200

    update models

--- tours/admin.py
+++ tours/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import Program, Category, Destination, City
+from .models import Program, Category, Destination, City, Hotel, Ticket
 
 class ProgramAdmin(admin.ModelAdmin):
-    list_display = ('id','title','arrival_date','departure_date','price_adt','flight','category','available_front_page','available_programs_page','available_lastminut_page','available_tours_page')
+    list_display = ('id','title','price_adt','arrival_date','departure_date','category','available_front_page','available_programs_page','available_lastminut_page','available_tours_page')
     list_display_links = ('id','title','arrival_date','price_adt')
     list_filter = ('arrival_date',)
     list_editable = ('available_front_page','available_programs_page','available_lastminut_page','available_tours_page')
-    search_fields = ('title','description','destination','city','district','flight','hotel_name')
+    search_fields = ('title','description','destination','city','district','arrival_date','hotel_name')
 
 admin.site.register(Category)
 admin.site.register(Destination)
 admin.site.register(Program, ProgramAdmin)
 admin.site.register(City)
+admin.site.register(Hotel)
+admin.site.register(Ticket)
--- tours/models.py
+++ tours/models.py
@@ -41,7 +41,39 @@ class City(models.Model):
         verbose_name_plural = 'cities'
 
     def __str__(self):
-        return self.title    
+        return self.title   
+
+class Ticket(models.Model):
+    airline_name = models.CharField(max_length=200,db_index=True)
+    airline_code =models.CharField(max_length=10)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200,unique=True)
+    
+    class Meta:
+        ordering = ('airline_code',)
+        verbose_name = 'Ticket'
+        verbose_name_plural = 'Tickets'
+
+    def __str__(self):
+        return self.airline_name
+
+    def get_absolute_url(self):
+        return reverse('tours:ticket_list', args=[self.slug])        
+
+class Hotel(models.Model):
+    title = models.CharField(max_length=200,db_index=True)
+    slug = models.SlugField(max_length=200,unique=True)
+    
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Hotel'
+        verbose_name_plural = 'Hotels'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tours:hotel_list', args=[self.slug])        
 
 def program_directory_path(instance, filename):
      
@@ -53,14 +85,14 @@ class Program(models.Model):
     city = models.ForeignKey(City, related_name='cit_programs', on_delete=models.CASCADE)
     title = models.CharField(max_length=300, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
+    description = models.TextField()
     arrival_date = models.DateField()
     departure_date = models.DateField()
-    description = models.TextField()
     price_adt = models.DecimalField(max_digits=12, decimal_places=2)
     price_chd_one = models.DecimalField(max_digits=10, decimal_places=2)
     price_chd_two = models.DecimalField(max_digits=10, decimal_places=2)
     price_inf = models.DecimalField(max_digits=10, decimal_places=2)
-    hotel_name = models.CharField(max_length=200)
+    hotel_name = models.ForeignKey(Hotel, related_name='htl_programs', on_delete=models.CASCADE)
     district = models.CharField(max_length=200)
     stars = models.IntegerField()
     image_main= models.ImageField(upload_to=program_directory_path, blank=True)
@@ -70,7 +102,7 @@ class Program(models.Model):
     image_4 = models.ImageField(upload_to=program_directory_path, blank=True)
     image_5 = models.ImageField(upload_to=program_directory_path, blank=True)
     image_6 = models.ImageField(upload_to=program_directory_path, blank=True)
-    flight = models.CharField(max_length=50)
+    flight = models.ForeignKey(Ticket, related_name='tkt_programs', on_delete=models.CASCADE)
     flight_detail = models.TextField()
     available_front_page = models.BooleanField(default=True)
     available_programs_page = models.BooleanField(default=True)
